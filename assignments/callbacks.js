f69774bf1d7d0133fb5f7ccc77975a4558e4ec66
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-08-20 14:43:36 -0700

    working on problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -60,13 +60,17 @@ function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
 }
-sumNums(5, 20, cb => {
+sumNums(25, 20, cb => {
   console.log(cb)
 });
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(25, 20, cb => {
+  console.log(cb)
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
